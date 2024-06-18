983c2241922645057f70f5c1b916b1590e82f129
kajal panchal <kajalpanchal@kajals-Air.home>
2024-06-17 22:33:56 -0700

    finish work

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css'
 
 function App() {
 const [gitHubName, setGitHubName] = useState('')      
-const [gitHubURL, setGitHubURL] = useState('')              
+const [gitHubURL, setGitHubURL] = useState('')   
+const [gitHubImageURL, setGitHubImageURL] = useState('')             
 
 
 useEffect(() => {
@@ -13,14 +14,21 @@ useEffect(() => {
     .then(data => {
         setGitHubName(data.name)
         setGitHubURL(data.html_url)
+        setGitHubImageURL(data.avatar_url)
     })
 }, [])
 
 return (
     <div className="App">
+      <div>
         <h1>Github Profile Info:</h1>
         <h2>{gitHubName}</h2>
         <a href={gitHubURL}><button>Link to GitHub profile</button></a>
+    </div>
+
+    <div className="pt-5">
+      <img src={gitHubImageURL} alt='Github profile image' width='200' height='200'/>
+    </div>
     </div>
 );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -12,12 +12,20 @@ beforeEach(() => {
     const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
     expect(gitHubName).toHaveTextContent('coder')
   })
-  test("receives GitHub name URL", async () => {
+  test("receives GitHub URL", async () => {
     fetch.mockResponseOnce(JSON.stringify({html_url: 'https://github.com/learningToCode1234'}))
     render(<App />)
-    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    const gitHubURL = await waitFor(() => screen.getByRole('Link', { level: 2 }))
+    expect(gitHubURL).toHaveTextContent('coder')
+  })
+  test("receives GitHub Image URL", async () => {
+    fetch.mockResponseOnce(JSON.stringify({avatar_url: 'https://avatars.githubusercontent.com/u/87375911?v=4'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByAltText('Github profile image'))
     expect(gitHubName).toHaveTextContent('coder')
   })
+
+  
 
 
   })
