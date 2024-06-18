b9e118028345e1bf9843cb448c4f438d916e0915
kajal panchal <kajalpanchal@kajals-Air.home>
2024-06-17 22:11:52 -0700

    create githubname and githuburl

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
+  describe('receives values from GitHub REST API using jest fetch mock', () => {
+  test("receives GitHub name", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+  })
+  test("receives GitHub name URL", async () => {
+    fetch.mockResponseOnce(JSON.stringify({html_url: 'https://github.com/learningToCode1234'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+  })
 
 
+  })
 
